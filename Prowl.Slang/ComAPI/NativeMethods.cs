690b1ef0386eb58f3e52ff0cd2dd7d1590c65094
Kai Angulo <kangriebling@gmail.com>
2025-05-08 01:10:57 -0700

    add native entry methods

---
+++ Prowl.Slang/ComAPI/NativeMethods.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Prowl.Slang.Native;
+
+
+internal static unsafe partial class SlangNative
+{
+    const string LibName = "slang";
+
+    [LibraryImport(LibName)]
+    public static partial SlangResult slang_createGlobalSession(int apiVersion, out IGlobalSession* outGlobalSession);
+
+    [LibraryImport(LibName)]
+    public static partial SlangResult slang_createGlobalSession2(SlangGlobalSessionDesc* desc, out IGlobalSession* outGlobalSession);
+
+    [LibraryImport(LibName)]
+    public static partial SlangResult slang_createGlobalSessionWithoutCoreModule(int apiVersion, out IGlobalSession* outGlobalSession);
+
+    [LibraryImport(LibName)]
+    public static partial void slang_shutdown();
+
+    [LibraryImport(LibName)]
+    public static partial byte* slang_getLastInternalErrorMessage();
+}
