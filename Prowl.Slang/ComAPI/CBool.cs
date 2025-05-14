1fb1187d9df37c4986e53ddad2bb943cd04eb6ab
Kai Angulo <kangriebling@gmail.com>
2025-05-13 20:08:54 -0700

    add CBool type

---
+++ Prowl.Slang/ComAPI/CBool.cs
@@ -0,0 +1,15 @@
+using System.Runtime.InteropServices;
+
+
+namespace Prowl.Slang.NativeAPI;
+
+
+[StructLayout(LayoutKind.Sequential)]
+public struct CBool
+{
+    private byte _value;
+
+    public static implicit operator bool(CBool cbool) => cbool._value == 1;
+
+    public static implicit operator CBool(bool nbool) => new() { _value = (byte)(nbool ? 1 : 0) };
+}
