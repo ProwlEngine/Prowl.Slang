8d1e62edbd1ce7819d5d6d350fbe585ffc2117a9
Kai Angulo <kangriebling@gmail.com>
2025-05-15 01:35:15 -0700

    Add InitializationException class

---
+++ Prowl.Slang/Managed/InitializationException.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace Prowl.Slang;
+
+
+public class InitializationException : Exception
+{
+    public InitializationException() { }
+    public InitializationException(string message) : base(message) { }
+    public InitializationException(string message, Exception inner) : base(message, inner) { }
+}
