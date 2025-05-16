2010317adf2d2e657939448ac3256ad74675d292
Kai Angulo <kangriebling@gmail.com>
2025-05-16 16:18:49 -0700

    Fix return type on IEntryPoint

--- Prowl.Slang/Native/Interfaces/IEntryPoint.cs
+++ Prowl.Slang/Native/Interfaces/IEntryPoint.cs
@@ -2,8 +2,7 @@ namespace Prowl.Slang.Native;
 
 
 [UUID(0x8f241361, 0xf5bd, 0x4ca0, 0xa3, 0xac, 0x2, 0xf7, 0xfa, 0x24, 0x2, 0xb8)]
-public interface IEntryPoint : IComponentType
+public unsafe interface IEntryPoint : IComponentType
 {
-    /* FunctionReflection */
-    void GetFunctionReflection();
+    FunctionReflection* GetFunctionReflection();
 }
