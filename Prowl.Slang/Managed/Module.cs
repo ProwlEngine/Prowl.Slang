0cf8fb708c70956d3500e1188893b1a77b9c7557
Kai Angulo <kangriebling@gmail.com>
2025-05-16 13:55:42 -0700

    hold parent session ref in module

--- Prowl.Slang/Managed/Module.cs
+++ Prowl.Slang/Managed/Module.cs
@@ -8,11 +8,16 @@ namespace Prowl.Slang;
 
 public unsafe class Module : ComponentType
 {
+    // Modules depend on a Session instance, but sessions dont depend on modules.
+    // Keep a ref to the session to prevent its underlying native ptr from being disposed while this module still exists.
+    internal Session _parentSession;
+
     internal IModule _module;
 
 
-    internal Module(IModule module) : base(module)
+    internal Module(IModule module, Session parent) : base(module)
     {
+        _parentSession = parent;
         _module = module;
     }
 
