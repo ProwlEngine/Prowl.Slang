dff5adee6afeb5b8dfbe30f29e19b8d00f8a4168
Kai Angulo <kangriebling@gmail.com>
2025-05-16 20:47:35 -0700

    Make Utility.cs return IEnumerable instead of IEnumerator

--- Prowl.Slang/Managed/Utility.cs
+++ Prowl.Slang/Managed/Utility.cs
@@ -2,12 +2,12 @@ using System;
 using System.Collections.Generic;
 
 
-namespace Prowl.Slang.Native;
+namespace Prowl.Slang;
 
 
 public static class Utility
 {
-    public static IEnumerator<T> For<T>(uint range, Func<uint, T> getter)
+    public static IEnumerable<T> For<T>(uint range, Func<uint, T> getter)
     {
         for (uint i = 0; i < range; i++)
             yield return getter.Invoke(i);
