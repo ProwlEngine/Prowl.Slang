040f222389620a41edb82316b74cc31f8a7b90d6
Kai Angulo <kangriebling@gmail.com>
2025-05-16 23:21:34 -0700

    Add 'ReadBytes' extension to ISlangBlob

--- Prowl.Slang/Native/Interfaces/ISlangBlob.cs
+++ Prowl.Slang/Native/Interfaces/ISlangBlob.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Runtime.InteropServices;
+
 namespace Prowl.Slang.Native;
 
 
@@ -18,5 +21,16 @@ public static class BlobExtensions
     public static unsafe string GetString(this ISlangBlob blob)
     {
         return System.Text.Encoding.UTF8.GetString((byte*)blob.GetBufferPointer(), (int)blob.GetBufferSize());
+    }
+
+
+    public static unsafe Memory<byte> ReadBytes(this ISlangBlob blob)
+    {
+        byte[] bytes = new byte[blob.GetBufferSize()];
+
+        fixed (byte* bytePtr = bytes)
+            NativeMemory.Copy(blob.GetBufferPointer(), bytePtr, (nuint)bytes.Length);
+
+        return bytes;
     }
 }
