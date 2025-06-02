d8ae510b3a04de5562992297affaeafb6bb5b7d9
Kai Angulo <kangriebling@gmail.com>
2025-06-01 21:07:59 -0700

    Fix GenericArgReflection overriding args when converting to native struct.

--- Slang/Reflection/GenericArgReflection.cs
+++ Slang/Reflection/GenericArgReflection.cs
@@ -1,28 +1,102 @@
 // This file is part of the Prowl Game Engine
 // Licensed under the MIT License. See the LICENSE file in the project root for details.
 
+using System;
+
 namespace Prowl.Slang;
 
 
-///
-public struct GenericArgReflection
+/// <summary>
+/// A generic type argument used when specializing a generic type.
+/// </summary>
+public readonly struct GenericTypeArgument
 {
-    ///
-    public TypeReflection TypeVal;
+    /// <summary>
+    /// The type of the generic argument.
+    /// </summary>
+    public readonly GenericArgType Type;
 
-    ///
-    public long IntVal;
+    /// <summary>
+    /// The generic argument value as a reflection type.
+    /// </summary>
+    public readonly TypeReflection TypeVal;
 
-    ///
-    public bool BoolVal;
+    /// <summary>
+    /// The generic argument value as an integer.
+    /// </summary>
+    public readonly long IntVal;
 
-    internal unsafe Native.GenericArgReflection ToNative()
+    /// <summary>
+    /// The generic argument value as a boolean.
+    /// </summary>
+    public readonly bool BoolVal;
+
+
+    /// <summary>
+    /// Creates a new generic type argument from a reflection type.
+    /// </summary>
+    public GenericTypeArgument(TypeReflection typeVal)
     {
-        return new()
-        {
-            typeVal = TypeVal._ptr,
-            intVal = IntVal,
-            boolVal = BoolVal
-        };
+        Type = GenericArgType.Type;
+        TypeVal = typeVal;
+        IntVal = 0;
+        BoolVal = false;
+    }
+
+
+    /// <summary>
+    /// Creates a new generic type argument from an integer value.
+    /// </summary>
+    public GenericTypeArgument(long intVal)
+    {
+        Type = GenericArgType.Int;
+        TypeVal = default;
+        IntVal = intVal;
+        BoolVal = false;
+    }
+
+
+    /// <summary>
+    /// Creates a new generic type argument from a boolean value.
+    /// </summary>
+    public GenericTypeArgument(bool boolVal)
+    {
+        Type = GenericArgType.Bool;
+        TypeVal = default;
+        IntVal = 0;
+        BoolVal = boolVal;
+    }
+
+
+    ///
+    public static implicit operator GenericTypeArgument(TypeReflection typeVal)
+    {
+        return new GenericTypeArgument(typeVal);
+    }
+
+
+    ///
+    public static implicit operator GenericTypeArgument(long intVal)
+    {
+        return new GenericTypeArgument(intVal);
+    }
+
+
+    ///
+    public static implicit operator GenericTypeArgument(bool boolVal)
+    {
+        return new GenericTypeArgument(boolVal);
+    }
+
+
+    internal unsafe Native.GenericArgReflection ToArgReflection()
+    {
+        if (Type == GenericArgType.Type)
+            return new() { typeVal = TypeVal._ptr };
+
+        if (Type == GenericArgType.Int)
+            return new() { intVal = IntVal };
+
+        return new() { boolVal = BoolVal };
     }
 }
