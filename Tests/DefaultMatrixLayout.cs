817adc589febeabec9996e8c42d5e973118bb2b0
Kai Angulo <kangriebling@gmail.com>
2025-06-01 22:51:13 -0700

    Add tests

---
+++ Tests/DefaultMatrixLayout.cs
@@ -0,0 +1,53 @@
+namespace Prowl.Slang.Test;
+
+
+public class DefaultMatrixLayout
+{
+    [Fact]
+    public void DefaultMatrixLayoutTest()
+    {
+        // Source for a module that contains an undecorated entrypoint.
+        const string userSourceBody =
+    """
+RWStructuredBuffer<float> output;
+
+[numthreads(1, 1, 1)][shader("compute")]
+void main(uniform float3x4 m)
+{
+    output[0] = m[0][0];
+}
+""";
+
+        TargetDescription targetDesc = new()
+        {
+            Format = CompileTarget.Glsl,
+            Profile = GlobalSession.FindProfile("glsl_460"),
+        };
+
+        SessionDescription sessionDesc = new()
+        {
+            Targets = [targetDesc],
+            DefaultMatrixLayoutMode = MatrixLayoutMode.ColumnMajor
+        };
+
+        Session session = GlobalSession.CreateSession(sessionDesc);
+
+        Module module = session.LoadModuleFromSourceString(
+            "m",
+            "m.slang",
+            userSourceBody,
+            out _);
+
+        EntryPoint entry = module.FindEntryPointByName("main");
+
+        ComponentType program = session.CreateCompositeComponentType([module, entry], out _);
+
+        ComponentType linkedProgram = program.Link(out _);
+
+        Memory<byte> outCode = linkedProgram.GetEntryPointCode(0, 0, out _);
+
+        string code = System.Text.Encoding.UTF8.GetString(outCode.Span);
+
+        Assert.Contains("row_major", code);
+    }
+}
---
+++ Tests/FcpwCompile.cs
@@ -0,0 +1,50 @@
+using System.Runtime.CompilerServices;
+
+namespace Prowl.Slang.Test;
+
+
+public class FcpwCompile
+{
+    static string GetScriptPath([CallerFilePath] string filePath = "") => Directory.GetParent(filePath)!.FullName;
+
+
+    [Fact]
+    public void FcpwCompileTest()
+    {
+        TargetDescription targetDesc = new()
+        {
+            Format = CompileTarget.Spirv,
+            Profile = GlobalSession.FindProfile("spirv_1_5"),
+        };
+
+        SessionDescription sessionDesc = new()
+        {
+            Targets = [targetDesc],
+
+            PreprocessorMacros = [new PreprocessorMacroDescription
+            {
+                Name = "_BVH_TYPE",
+                Value = "2",
+            }],
+
+            FileProvider = new FileProvider(),
+            SearchPaths = [Path.Join(GetScriptPath(), "Shaders")]
+        };
+
+        Session session = GlobalSession.CreateSession(sessionDesc);
+
+        Module module = session.LoadModule("fcpw/bvh-traversal.cs.slang", out _);
+
+        EntryPoint entryPoint = module.FindEntryPointByName("rayIntersection");
+
+        ComponentType compositeProgram = session.CreateCompositeComponentType(
+            [module, entryPoint],
+            out _);
+
+        ComponentType linkedProgram = compositeProgram.Link(out _);
+
+        Memory<byte> code = linkedProgram.GetEntryPointCode(0, 0, out _);
+
+        Assert.NotEqual(0, code.Length);
+    }
+}
